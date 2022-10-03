42c8e4b604e15ad64460f3fd88c859f33b795471
abhattacharjee <abhattacharjee@socivolta.com>
2022-10-02 23:49:54 -0400

    detecting best random config

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-const canvas=document.getElementById("myCanvas");
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=200;
 
-canvas.width=200;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=300;
 
-const ctx=canvas.getContext("2d");
-const road=new Road(canvas.width/2,canvas.width*0.9);
-const car=new Car(road.getLaneCenter(1),100,30,50,"AI");
+
+
+const carCtx=carCanvas.getContext("2d");
+const networkCtx=networkCanvas.getContext("2d");
+
+const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
+//const car=new Car(road.getLaneCenter(1),100,30,50,"AI");
+const N=100;
+const cars=generateCars(N)
+
 const traffic=[
     new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",1)
 ];
@@ -12,21 +21,54 @@ const traffic=[
 //car.draw(ctx);
 animate ()
 
-function animate(){    
+function generateCars(N){
+    const cars=[];
+    for (let i=1;i<=N;i++){
+        cars.push(
+            new Car(
+                road.getLaneCenter(1),
+                100,30,50,"AI"
+
+            )
+        );
+    }
+    return cars;
+
+}
+
+
+function animate(time){    
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
-    car.update(road.borders,traffic);
-    canvas.height=window.innerHeight;
 
-    ctx.save()
-    ctx.translate(0,-car.y+canvas.height*0.7)
-    road.draw(ctx);
-    car.draw(ctx,"blue");
-    for(let i=0;i<traffic.length;i++){
-        traffic[i].draw(ctx,"red");
+    for (let i=0;i<cars.length;i++){
+        cars[i].update(road.borders,traffic);
     }
-    ctx.restore();
+    const besatCar=cars.find(
+        c=>c.y==Math.min(...cars.map(c=>c.y))
+
+    );
+    carCanvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
+
+    carCtx.save()
+    carCtx.translate(0,-besatCar.y+carCanvas.height*0.7)
+    road.draw(carCtx);
+    carCtx.globalAlpha=0.2;
+
+    for (let i=0;i<cars.length;i++){
+        cars[i].draw(carCtx,"blue");
+    }
+    carCtx.globalAlpha=1;
+    besatCar.draw(carCtx,"blue",true);
+
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(carCtx,"red");
+    }
+    carCtx.restore();
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx,besatCar.brain);
     requestAnimationFrame(animate)
 
 }
