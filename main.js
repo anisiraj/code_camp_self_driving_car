19b34aec9a65fa7dc7d7af6058e330e2c48bb738
abhattacharjee <abhattacharjee@socivolta.com>
2022-10-03 10:11:37 -0400

    added saving best network and mutation

--- main.js
+++ main.js
@@ -12,14 +12,50 @@ const networkCtx=networkCanvas.getContext("2d");
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 //const car=new Car(road.getLaneCenter(1),100,30,50,"AI");
 const N=100;
-const cars=generateCars(N)
+const cars=generateCars(N);
+let besatCar=cars[0];
+
+if(localStorage.getItem("bestBrain")){
+    for (let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(localStorage.getItem("bestBrain"));
+        if (i!=0){
+            NeuralNetwork.mutate(cars[i].brain,0.1)
+        }
+
+
+    }
+   
+}
 
 const traffic=[
-    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",1)
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-900,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-900,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-1100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-1100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-1300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-1300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-1500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-1700,30,50,"DUMMY",2),
+    
 ];
 
 //car.draw(ctx);
 animate ()
+function save(){
+    localStorage.setItem("bestBrain",JSON.stringify(besatCar.brain));
+
+}
+function discard(){
+    localStorage.removeItem("bestBrain");
+
+}
 
 function generateCars(N){
     const cars=[];
@@ -45,7 +81,7 @@ function animate(time){
     for (let i=0;i<cars.length;i++){
         cars[i].update(road.borders,traffic);
     }
-    const besatCar=cars.find(
+    besatCar=cars.find(
         c=>c.y==Math.min(...cars.map(c=>c.y))
 
     );
